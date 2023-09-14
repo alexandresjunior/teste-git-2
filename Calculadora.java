c1770a8b69662314bb96efcc8656169527f04a8c
Alexandre de Souza Jr. <83607914+alexandresjunior@users.noreply.github.com>
2023-09-14 20:48:57 -0300

    Cálculo da soma de dois números

---
+++ Calculadora.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double num2 = sc.nextDouble();
+
+        sc.close();
+
+        double soma = num1 + num2;
+
+        System.out.println("A soma dos números é: " + soma);
+    }
+}
